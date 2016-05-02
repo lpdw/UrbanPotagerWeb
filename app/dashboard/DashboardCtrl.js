158eb94ac7f390f49a4f0a3efc2d7240beb78c91
auroret <aurore.troubat@gmail.com>
2016-05-02 21:55:49 +0200

    Preparation dashboards

--- app/dashboard/DashboardCtrl.js
+++ app/dashboard/DashboardCtrl.js
@@ -6,12 +6,34 @@ angular.module('myApp.dashboard', ['ngRoute'])
         $routeProvider.when('/dashboard', {
             templateUrl: 'dashboard/dashboard.html',
             controller: 'DashboardCtrl as dashboard'
+        }).when('/potager', {
+            templateUrl: 'dashboard/potager/potager.html',
+            controller: 'PotagerCtrl as potager'
         });
     }])
 
-    .controller('DashboardCtrl', [function() {
+    .controller('DashboardCtrl', ['$location', function($location) {
 
         var vm = this;
+        vm.title = "Mes potagers";
+        
+        vm.potagers = [{
+            name: "Potager 1",
+            temperature: "32°",
+            humidite: "ok"
+        },{
+            name: "Potager 2",
+            temperature: "30°",
+            humidite:"NOK"
+        }];
 
-        vm.title = "Page dashboard";
+        /**
+         * Redirige l'utilisateur sur le potager sélectionné
+         * @param p
+         */
+        vm.selectedPotager = function(p){
+            $location.path('/potager/').search({param: p});
+        };
+
+        
     }]);
---
+++ app/dashboard/potager/PotagerCtrl.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    function PotagerCtrl($location) {
+
+        var vm = this;
+
+        vm.potager = $location.search().param;
+        vm.title = "Potager:" + vm.potager.name;
+
+    }
+
+    angular.module('myApp.dashboard').controller('PotagerCtrl', PotagerCtrl);
+
+}());
