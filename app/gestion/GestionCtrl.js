ed7800a7cf0399b02189d78eaf8d5913782b2aab
NBrocheray <noemybrocheray@gmail.com>
2016-05-04 11:41:07 +0200

    Home Page

--- app/gestion/GestionCtrl.js
+++ app/gestion/GestionCtrl.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 angular.module('myApp.gestion', ['ngRoute', 'myApp.services', 'ngResource'])
@@ -14,77 +15,8 @@ angular.module('myApp.gestion', ['ngRoute', 'myApp.services', 'ngResource'])
         var vm = this;
         vm.title = "Page gestion";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        vm.potagers = [
-            {
-                title:"potager 1",
-                description:"super potager 1",
-                owner: 'JD',
-                place:'Tour Eiffel',
-                plants:[
-                    {
-                        title:'Rose',
-                        number:'4'
-                    },
-                    {
-                        title:'Tulipe',
-                        number:'6'
-                    },
-                    {
-                        title:'Arôme',
-                        number:'2'
-                    }
-                ]
-            },
-            {
-                title:"potager 2",
-                description:"super potager 2",
-                owner: 'JD',
-                place:'FacLab',
-                plants:[
-                    {
-                        title:'Pommes de terres',
-                        number:'4'
-                    },
-                    {
-                        title:'Tulipe',
-                        number:'6'
-                    },
-                    {
-                        title:'Laitue',
-                        number:'2'
-                    }
-                ]
-            },
-            {
-                title:"potager 3",
-                description:"super potager 3",
-                owner: 'JD',
-                place:'Terril',
-                plants:[
-                    {
-                        title:'Frites',
-                        number:'4'
-                    },
-                    {
-                        title:'Marguerite',
-                        number:'5'
-                    }
-                ]
-            }
-        ]
-    }]);
-=======
-=======
->>>>>>> origin/Dev
         PotagerService.resource.query(function (datas) {
             vm.potagers = datas;
         });
 
-<<<<<<< HEAD
     });
->>>>>>> master
-=======
-    });
->>>>>>> origin/Dev
--- app/home/HomeCtrl.js
+++ app/home/HomeCtrl.js
@@ -12,20 +12,10 @@ angular.module('myApp.home', ['ngRoute'])
     .controller('HomeCtrl', [function() {
 
         var vm = this;
-<<<<<<< HEAD
+
         vm.title = "UrbanPotager";
         vm.imageUrl = "http://urbanaplant.com/img/cms/cat-smart-garden.png";
         vm.description = "Liquorice biscuit dragée. Ice cream ice cream brownie. Tart jelly beans bonbon cotton candy pastry tiramisu. Brownie chocolate bar chocolate cake. Bear claw carrot cake biscuit cake. Cake macaroon toffee dessert donut.";
         vm.imageUrl1 = "http://urbanaplant.com/img/cms/cat-smart-garden.png";
         vm.description1 = "Tiramisu cookie oat cake muffin. Pudding marshmallow chupa chups liquorice marzipan chocolate cake soufflé. Sesame snaps soufflé liquorice ice cream croissant sweet fruitcake. Biscuit halvah sesame snaps chocolate caramels.";
     }]);
-=======
-
-        vm.title = "Bienvenue sur UrbanPotager !";
-        vm.imageUrl = "images/home-img1.png";
-        vm.imageDesc = "Bacon ipsum dolor amet bacon jerky salami spare ribs tail. Boudin pork loin alcatra hamburger picanha porchetta capicola. Bacon ipsum dolor amet bacon jerky salami spare ribs tail."
-        vm.imageUrl1 = "images/home-img2.png";
-        vm.imageDesc1 = "Corned beef porchetta pork loin alcatra salami kevin pork chop. Chicken shoulder flank pastrami, fatback t-bone ham meatball shank bresaola sirloin ham hock cow."
-
-    }])
->>>>>>> master
