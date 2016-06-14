12385ddca3591af890e3e6abbc0c18eb558f6b41
auroret <aurore.troubat@gmail.com>
2016-06-14 22:34:18 +0200

    Ajout images dashboard/Post configuration service/Prise en compte token auth

--- app/dashboard/DashboardCtrl.js
+++ app/dashboard/DashboardCtrl.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    function DashboardCtrl($location, PotagerService) {
+    function DashboardCtrl($location, PotagerService, ConfigurationService) {
 
         var vm = this;
         vm.title = "MES POTAGERS";
@@ -18,6 +18,8 @@
             });
         };
 
+
+
         /**
          * Gestion des images aléatoires des dash
          */
@@ -28,8 +30,37 @@
             }
         };
 
+        vm.addConfigTest = function () {
+            var config = {
+                "id": 0,
+                "name": "configTest1",
+                "description": "Une description de test bien longue qui devrait faire péter l'ihm si c'est mal codé ce qui est surement le cas",
+                "slug": "configTest1",
+                "lightTreshold": 3,
+                "lightingStart": {
+                    "hour": 15, 
+                    "minute": 37
+                },
+                "lightingEnd": {
+                    "hour": 4, 
+                    "minute": 15
+                },
+                "isWategringActive": 1,
+                "wateringStart": 4,
+                "wateringEnd": 3
+            };
+            /*ConfigurationService.resource.get(function (datas) {
+                console.log("retour get",datas);
+
+            });*/
+            ConfigurationService.resource2.post(config, function (datas) {
+                    console.log("retour post",datas);
+            });
+
+        };
+
         vm.getImageRandom = function () {
-            var temp = Math.floor((Math.random() * 12) + 1);
+            var temp = Math.floor((Math.random() * 18) + 1);
             return "assets/images/dashboards/"+ temp +".png";
         };
 
--- app/dashboard/services/ConfigurationService.js
+++ app/dashboard/services/ConfigurationService.js
@@ -4,18 +4,33 @@
     function ConfigurationService($resource){
 
         var apiPath = 'https://urbanpotager.labesse.me';
+        var token =  '';
 
         var resource = $resource(apiPath+'/garden/:slugGarden/configuration', {slugGarden: '@slugGarden'}, {
-            get:{
-                method:"GET",
-                headers:{ Accept: 'text/html, application/json, text/plain, */*' }
+            post:{
+                method:"POST",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*',
+                    Authorization: 'Bearer '+ token
+                }
             },
             update: {
                 method: 'PUT'
             }
         });
+
+        var resource2 = $resource(apiPath+'/configurations', {}, {
+            post:{
+                method:"POST",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*' ,
+                    Authorization: 'Bearer '+ token
+                }
+            }
+        });
         return {
-            resource: resource
+            resource: resource,
+            resource2: resource2
         };
     }
     controllers.factory('ConfigurationService', ConfigurationService);
