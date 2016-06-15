5b976e44e863dcb35a5e184c12900ea8c5db4550
auroret <aurore.troubat@gmail.com>
2016-06-15 19:46:14 +0200

    Service config wip

--- app/dashboard/services/ConfigurationService.js
+++ app/dashboard/services/ConfigurationService.js
@@ -4,7 +4,7 @@
     function ConfigurationService($resource){
 
         var apiPath = 'https://urbanpotager.labesse.me';
-        var token =  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE0NjYwOTEzOTYsInVzZXJuYW1lIjoiYWRtaW4iLCJpYXQiOiIxNDY2MDA0OTk2In0.U19-u2LAaPIYwplLr9Qbofa4Cn1x6UAgBnPOvnyuUktL8d1j-bBVnurHdpR9AN-zUB2JOuBn5MrU9wkE2NF_99e0_8wMc2H5sNxctr013AI374TFYxqR_3Nge_hNP9FWMrIBynJQdfP1dpbypTcI9V3nFH-dyeIpNmR2p3BoWuBZ8TtAT8uwYOcKnRqSKfjr2xs7fn2uLwLEggTBcJKvX6uRYe9dPujxAwSGyYph77g9VS6nMw7apnsYkZRDO3mJFtMPiwDAEFh2yZVPhYq2VUqmsA6jH2lw6AgF0wkLEmjmFSX3bhRtIlcyxu5_NjodbTHKMBQbk52abUmPrDCuljJkBsMUQm3ZMgx4k8yh8G4A8p239KvOzlZZuE5c9scVaaF4HZ3oWU_LdUBjX3jKt5C1DDincb6PbCO-y-YYa8WEtjm-PRIvBFvBvEC63ZGeZnHNSPxBzGdbZJKsQ1cpdFCxLeg6krMvhKw32Tkwr44WuuLHFKcanPqXZdAVs9pwP26Esrb_uC1DBnPosZIb_ev7CVlMCmLtUnXLinrfjoilBcitoqGkQZxFvPA9k4GCVk9jNI-s4BCunk-AxMW3FeDXtdUXvhezVTvVqXhr42yrcx5cIVt1hrilYIaFka420wVNE0sGkorBjY5OdcHsH1TJRk7keO7jfGF8tNdhDJs';
+        var token =  '';
 
         var resource = $resource(apiPath+'/garden/:slugGarden/configuration/:slugConfiguration', {
                             slugGarden: '@slugGarden',
