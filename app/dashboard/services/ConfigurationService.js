bca0fe760eaa879a9f50548fcbe0ef834cafab5d
MatthieuCt <cayet.matthieu@gmail.com>
2016-06-15 21:58:29 +0200

    configurationService

--- app/dashboard/services/ConfigurationService.js
+++ app/dashboard/services/ConfigurationService.js
@@ -6,6 +6,8 @@
         var apiPath = 'https://urbanpotager.labesse.me';
         var token =  localStorageService.get('token');
 
+
+
         var resource = $resource(apiPath+'/garden/:slugGarden/configuration/:slugConfiguration', {
                             slugGarden: '@slugGarden',
                             slugConfiguration: '@slugConfiguration'
@@ -30,13 +32,16 @@
         });
 
         var resource2 = $resource(apiPath+'/configurations', {}, {
+            //Post new configuration
             post:{
                 method:"POST",
                 headers:{
                     Accept: 'text/html, application/json, text/plain, */*' ,
                     Authorization: 'Bearer '+ token
                 }
-            }, get:{
+            },
+            //Get configurations for current user
+            get:{
                 method:"GET",
                 headers:{
                     Accept: 'text/html, application/json, text/plain, */*' ,
@@ -44,9 +49,77 @@
                 }
             }
         });
+
+        var resource3 = $resource(apiPath+'/configurations/:slug', {
+            slug: '@slug'
+        }, {
+            //Get configuration details
+            get:{
+                method:"GET",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*' ,
+                    Authorization: 'Bearer '+ token
+                }
+            },
+            //Modify a configuration
+            patch:{
+                method:"PATCH",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*' ,
+                    Authorization: 'Bearer '+ token
+                }
+            },
+            //Delete a configuration
+            delete:{
+                method:"DELETE",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*' ,
+                    Authorization: 'Bearer '+ token
+                }
+            }
+        });
+
+        var resource4 = $resource(apiPath+'/gardens/:slugGarden/configurations', {
+            slugGarden: '@slugGarden'
+        }, {
+            //Get garden configuration
+            get:{
+                method:"GET",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*' ,
+                    Authorization: 'Bearer '+ token
+                }
+            },
+            //Unlink garden with current configuration
+            delete:{
+                method:"DELETE",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*' ,
+                    Authorization: 'Bearer '+ token
+                }
+            }
+        });
+
+        var resource5 = $resource(apiPath+'/gardens/:slugGarden/configurations/:slugConfiguration', {
+            slugGarden: '@slugGarden',
+            slugConfiguration: '@slugConfiguration'
+        }, {
+            //Get garden configuration
+            post:{
+                method:"POST",
+                headers:{
+                    Accept: 'text/html, application/json, text/plain, */*' ,
+                    Authorization: 'Bearer '+ token
+                }
+            }
+        });
+
         return {
             resource: resource,
-            resource2: resource2
+            resource2: resource2,
+            resource3: resource3,
+            resource4: resource4,
+            resource5: resource5
         };
     }
     controllers.factory('ConfigurationService', ConfigurationService);
